7205b1e0053a8395ae037ffb5bffbe11d3ee22c7
SAIKO9X <cardosed3@gmail.com>
2023-01-21 17:45:06 -0300

    add README.md

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 function toggleMode() {
 	const body = document.body;
 
-	// if (body.classList.contains("light")) {
-	// 	body.classList.remove("light");
-	// } else {
-	// 	body.classList.add("light");
-	// }
-
-	// A função toggle ira fazer a mesma coisa que o código acima, ela verifica se dentro da tag body existe uma classe chamada "light"
-
 	body.classList.toggle("light");
+	// A função toggle ira fazer a mesma coisa que o código abaixo, ela verifica se dentro da tag body existe uma classe chamada "light"
+
+	// pegar a  tag img
+	const profile = document.querySelector("#profile-section img");
+
+	//substituir a imagem através do setAtribute
+	if (body.classList.contains("light")) {
+		profile.setAttribute("src", "./assets/profile.jpg");
+	} else {
+		profile.setAttribute("src", "./assets/profile-dark.jpg");
+	}
 }
